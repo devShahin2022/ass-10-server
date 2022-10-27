8ef4ebf63a8adf04b22f6632d25ff0b2c6668d30
Shahin Alam <shahinzzz1949@gmail.com>
2022-10-27 12:50:05 +0600

    serve form github fir vercel

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const fakeData = require('./courseFakeData.json');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+
+app.get('/',(req,res) => {
+    res.send('Server running');
+});
+// get all course data
+app.get('/all-course',(req,res) => {
+    res.send(fakeData);
+});
+
+// get single data
+app.get('/all-course/:id',(req,res) => {
+    const id = parseInt(req.params.id);
+    const singleData = fakeData.find(course => course.id === id) || [];
+    res.send(singleData);
+});
+
+app.listen(port, ()=>{
+    console.log(`Server running on port : ${port}`);
+})
